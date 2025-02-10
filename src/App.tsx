702f66534cfa994872c63942ea659866d82b0a64
Mauricio Antero <mjunior447@gmail.com>
2025-02-09 23:43:03 -0300

    fix: responsiveness fix for ultriwide screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import DraftEditor from "./components/DraftEditor/DraftEditor";
 
 function App() {
   return (
-    <main className="flex flex-col gap-12 sm:gap-20 w-full h-screen p-4 sm:p-12 bg-[#FFFAF0]">
-      <div>
-        <h1 className="text-lg sm:text-2xl font-bold">
-          Bem-vinda(o) ao DraftComposer!
-        </h1>
-        <h2>Crie seu esboço de redação</h2>
-      </div>
-      <article>
-        <DraftEditor />
+    <main className="flex flex-col gap-12 sm:gap-20 w-full h-screen p-4 sm:p-12 bg-[#FFFAF0] items-center">
+      <article className="flex flex-col max-w-3xl gap-12">
+        <div>
+          <h1 className="text-lg sm:text-2xl font-bold">
+            Bem-vinda(o) ao DraftComposer!
+          </h1>
+          <h2>Crie seu esboço de redação</h2>
+        </div>
+        <div>
+          <DraftEditor />
+        </div>
       </article>
     </main>
   );
